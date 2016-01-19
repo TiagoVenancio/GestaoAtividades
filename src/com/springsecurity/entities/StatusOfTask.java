7859f015b06e474d3b5595915f417e296d0214e7
GabrielGMT <gabrieltrombelli@outlook.com>
2016-01-19 09:46:16 -0200

    Class StatusOfTask

--- src/com/springsecurity/entities/StatusOfTask.java
+++ src/com/springsecurity/entities/StatusOfTask.java
@@ -1,5 +1,83 @@
 package com.springsecurity.entities;
 
-public class StatusOfTask {
+import java.io.Serializable;
+import java.util.Calendar;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.springsecurity.enums.StatusTaskEnum;
+
+@Entity
+@Table(name = "sg_act_status_task", schema = "gestao_atividades")
+public class StatusOfTask implements Serializable {
+	private static final long serialVersionUID = -7590317347612436291L;
+
+	private Long id;
+	private Calendar createDate;
+	private Calendar lastUpdate;
+	private StatusTaskEnum statusTaskEnum;
+	private String description;
+	private String finalAction;
+
+	@Id
+	@GeneratedValue
+	@Column(name = "STATUS_TASK_ID", length = 20)
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;	
+	}
+
+	@Column (name = "CREATE_DATE", nullable = false )
+	public Calendar getCreateDate() {
+		return createDate;
+	}
+
+	public void setCreateDate(Calendar createDate) {
+		this.createDate = createDate;
+	}
+
+	@Column (name = "LAST_UPDATE", nullable = false)
+	public Calendar getLastUpdate() {
+		return lastUpdate;
+	}
+
+	public void setLastUpdate(Calendar lastUpdate) {
+		this.lastUpdate = lastUpdate;
+	}
+
+	@Column (name = "STATUS_OBJECT_TASK", length = 10, nullable = false)
+	public StatusTaskEnum getStatusTaskEnum() {
+		return statusTaskEnum;
+	}
+
+	public void setStatusTaskEnum(StatusTaskEnum statusTaskEnum) {
+		this.statusTaskEnum = statusTaskEnum;
+	}
+
+	@Column(name = "DESCRIPTION", length = 45, nullable = false)
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	@Column (name = "FINAL_ACTION", length = 5, nullable = false)
+	public String getFinalAction() {
+		return finalAction;
+	}
+
+	public void setFinalAction(String finalAction) {
+		this.finalAction = finalAction;
+	}
+
+	
 }
