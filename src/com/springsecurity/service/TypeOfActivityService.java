b0abd267528411df1c31caccc7167981c1c6e8b1
GabrielGMT <gabrieltrombelli@outlook.com>
2016-01-26 23:29:22 -0200

    TypeOfActivityBean

--- src/com/springsecurity/service/TypeOfActivityService.java
+++ src/com/springsecurity/service/TypeOfActivityService.java
@@ -1,12 +1,15 @@
 package com.springsecurity.service;
 
 import java.util.List;
+
 import com.springsecurity.entities.TypeOfActivity;
 
 public interface TypeOfActivityService {
 
 	void adicionar(TypeOfActivity typeOfActivity) throws IllegalArgumentException;
+	
+	void alterar(TypeOfActivity typeOfActivity) throws IllegalArgumentException;
 
-	List<TypeOfActivity> findAll();
+	public List<TypeOfActivity> getAllTipoDeAtividades();
 
 }
