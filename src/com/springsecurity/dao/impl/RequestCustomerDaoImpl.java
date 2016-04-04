4f694e7696be0e49a7da962e4945b9b5132ced15
TiagoVenancio <tiago.venancio@outlook.com>
2016-04-04 19:08:44 -0300

    Correção das class DAO (Task e Customer) e Alteração das dimensões pages.

--- src/com/springsecurity/dao/impl/RequestCustomerDaoImpl.java
+++ src/com/springsecurity/dao/impl/RequestCustomerDaoImpl.java
@@ -1,60 +1,43 @@
 package com.springsecurity.dao.impl;
 
 import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import org.hibernate.Criteria;
-import org.hibernate.Session;
-import org.hibernate.criterion.Restrictions;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
-import com.springsecurity.dao.RequestTaskDao;
-import com.springsecurity.entities.RequestTask;
+import com.springsecurity.dao.RequestCustomerDao;
+import com.springsecurity.entities.RequestCustomer;
 
 @Repository
 @Transactional
-public class RequestCustomerDaoImpl implements RequestTaskDao {
+public class RequestCustomerDaoImpl implements RequestCustomerDao {
 
 	@PersistenceContext
 	private EntityManager entityManager;
-	private Session session;
 
 	@Override
-	public RequestTask getById(final Long id) {
-		return entityManager.find(RequestTask.class, id);
+	public RequestCustomer getById(Long id) {
+		return entityManager.find(RequestCustomer.class, id);
 	}
 
 	@SuppressWarnings("unchecked")
 	@Override
-	public List<RequestTask> findAll() {
-		return entityManager.createQuery("FROM " + RequestTask.class.getName())
-				.getResultList();
+	public List<RequestCustomer> findAll() {
+		return entityManager.createQuery(
+				"FROM " + RequestCustomer.class.getName()).getResultList();
 	}
 
 	@Override
-	public void save(RequestTask requestTask) {
-		entityManager.persist(requestTask);
+	public void save(RequestCustomer requestCustomer) {
+		entityManager.persist(requestCustomer);
 
 	}
 
 	@Override
-	public void update(RequestTask requestTask) {
-		entityManager.merge(requestTask);
+	public void update(RequestCustomer requestCustomer) {
+		entityManager.merge(requestCustomer);
 
-	}
-
-	@SuppressWarnings("unchecked")
-	@Override
-	public List<RequestTask> buscarTaskPor(RequestTask requestTask) {
-		Criteria cr = session.createCriteria(RequestTask.class);
-		cr.add(Restrictions.like("resume", "%resume%"));
-		cr.add(Restrictions.eq("typeOfActivity", requestTask.getTypeOfActivity()));
-		cr.add(Restrictions.eq("typeOfSubActivity",	requestTask.getTypeOfActivity())); // ALTERAR AQUI PARA SUB-ATIVIDADE
-		cr.add(Restrictions.between("conclusionDate",requestTask.getConclusionDate(),requestTask.getConclusionDate()));
-
-		List<RequestTask> results = cr.list();
-
-		return results;
 	}
 
 }
--- src/com/springsecurity/dao/impl/RequestTaskDaoImpl.java
+++ src/com/springsecurity/dao/impl/RequestTaskDaoImpl.java
@@ -1,45 +1,65 @@
 package com.springsecurity.dao.impl;
 
 import java.util.List;
-
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
-
-import com.springsecurity.dao.RequestCustomerDao;
-import com.springsecurity.entities.RequestCustomer;
+import com.springsecurity.dao.RequestTaskDao;
+import com.springsecurity.entities.RequestTask;
 
 @Repository
 @Transactional
-public class RequestTaskDaoImpl implements RequestCustomerDao {
+public class RequestTaskDaoImpl implements RequestTaskDao {
 
 	@PersistenceContext
 	private EntityManager entityManager;
+	private Session session;
 
+	@SuppressWarnings("unchecked")
 	@Override
-	public RequestCustomer getById(final Long id) {
-		return entityManager.find(RequestCustomer.class, id);
+	public List<RequestTask> findAll() {
+		return entityManager.createQuery("FROM " + RequestTask.class.getName())
+				.getResultList();
 	}
 
 	@SuppressWarnings("unchecked")
 	@Override
-	public List<RequestCustomer> findAll() {
-		return entityManager.createQuery(
-				"FROM " + RequestCustomer.class.getName()).getResultList();
+	public List<RequestTask> buscarTaskPor(RequestTask requestTask) {
+		Criteria cr = session.createCriteria(RequestTask.class);
+		cr.add(Restrictions.like("resume", "%resume%"));
+		cr.add(Restrictions.eq("typeOfActivity",
+				requestTask.getTypeOfActivity()));
+		cr.add(Restrictions.eq("typeOfSubActivity",
+				requestTask.getTypeOfActivity())); // ALTERAR AQUI PARA
+													// SUB-ATIVIDADE
+		cr.add(Restrictions.between("conclusionDate",
+				requestTask.getConclusionDate(),
+				requestTask.getConclusionDate()));
+
+		List<RequestTask> results = cr.list();
+
+		return results;
 	}
 
 	@Override
-	public void save(RequestCustomer requestCustomer) {
-		entityManager.persist(requestCustomer);
+	public void save(RequestTask requestTask) {
+		entityManager.persist(requestTask);
 
 	}
 
 	@Override
-	public void update(RequestCustomer requestCustomer) {
-		entityManager.merge(requestCustomer);
+	public void update(RequestTask requestTask) {
+		entityManager.merge(requestTask);
 
+	}
+
+	@Override
+	public RequestTask getById(Long id) {
+		return entityManager.find(RequestTask.class, id);
 	}
 
 }
